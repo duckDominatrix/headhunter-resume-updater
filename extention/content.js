186e8ad53aa2c3a4a94cb72841b8c3489cbe22c0
Oleg Kononov <kononov.itwork@gmail.com>
2019-11-01 22:53:59 +0300

    Fix mutation track(delete attributes track, and add childList track in configure observer), refactor code and comments

--- extention/content.js
+++ extention/content.js
@@ -1,34 +1,39 @@
-console.log('start');
-
 const trackedSelector = '.applicant-resumes-action_no-mobile';
 const buttonSelector = '.applicant-resumes-update-button';
 
 // Select target elements for tracking(wich displayed on desktop fersion)
 const elements = Array.from(document.querySelectorAll(trackedSelector));
-// that elements have update button as a child or subChild in target element
+
+// Filtering the elements that have update button as a child or sub child in target element
 const trackedElements = elements.filter((element) => element.querySelector(buttonSelector));
 
 // Configure observer (what mutations track)
 const config = {
-  attributes: true,
+  childList: true,
   subtree: true,
 };
 
-// Chek element that has update button enable(not disable) and push it
-const pushButtonIfEnable = (element, selector) => {
-  const { className } = element.querySelector(selector);
+// Checking the class of an element that it is not disabled
+const hasNotDisable = (className) => !className.includes('disable');
 
-  if (!className.includes('disable')) {
-    element.querySelector('button').click();
+// Checking the element that has update button enable(not disable) and push it
+function pushButtonIfEnable(element, selector) {
+  const { className } = element.querySelector(selector);
+  if (hasNotDisable(className)) {
+    const button = element.querySelector('button');
+    button.click();
   }
-};
+}
 
 // Callback function which to be called
-const updateButtonPush = (mutations) => {
-  mutations.forEach(() => {
-    trackedElements.forEach((tracked) => pushButtonIfEnable(tracked, buttonSelector));
+function updateButtonPush(mutations) {
+  mutations.forEach((mutation) => {
+    const { type, target, addedNodes } = mutation;
+    if (type && addedNodes.length) {
+      pushButtonIfEnable(target, buttonSelector);
+    }
   });
-};
+}
 
 // Create an observer instance with callback function
 const observer = new MutationObserver(updateButtonPush);
@@ -37,6 +42,5 @@ const observer = new MutationObserver(updateButtonPush);
 // Getting started monitoring target element changes by MutationObserver
 trackedElements.forEach((tracked) => {
   pushButtonIfEnable(tracked, buttonSelector);
-
   observer.observe(tracked, config);
 });
